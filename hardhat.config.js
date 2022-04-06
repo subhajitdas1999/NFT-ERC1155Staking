c0e197469a13f8df9f5f509f0f87dc9da184b48c
Subhajit Das <sd651846@gmail.com>
2022-04-06 18:29:33 +0530

    added rinkeby network details and etherscan api

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,18 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config({ path: "./.env" });
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.13",
+  networks: {
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [`${process.env.RINKEBY_PRIVATE_KEY}`],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
